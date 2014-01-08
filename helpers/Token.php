a46b99e0c41142d804e98ab555fa6c9916e56cab
Oleg Grigoriev <go.vasac@gmail.com>
2014-01-08 17:57:59 +0400

    helper\Token: The class of a single token

---
+++ helpers/Token.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @package axy\ml
+ */
+
+namespace axy\ml\helpers;
+
+/**
+ * The class of a single token
+ *
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ *
+ * @property-read array $subs
+ * @property-read string $name
+ * @property-read string $content
+ */
+class Token extends \stdClass
+{
+    const TYPE_HEADER = 'header';
+    const TYPE_ANCHOR = 'anchor';
+    const TYPE_BLOCK = 'block';
+    const TYPE_TEXT = 'text';
+    const TYPE_TAG = 'tag';
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var int
+     */
+    public $line;
+
+    /**
+     * Constructor
+     *
+     * @param string $type
+     * @param int $line [optional]
+     */
+    public function __construct($type, $line = null)
+    {
+        $this->type = $type;
+        $this->line = $line;
+    }
+
+    /**
+     * Append token to subs
+     *
+     * @param \axy\ml\helpers\Token $token
+     */
+    public function append(Token $token)
+    {
+        if (empty($this->subs)) {
+            $this->subs = [];
+        }
+        $this->subs[] = $token;
+    }
+
+    /**
+     * Get list of subs
+     *
+     * @return array
+     */
+    public function getSubs()
+    {
+        return empty($this->subs) ? [] : $this->subs;
+    }
+
+    /**
+     * Represent the token as array
+     *
+     * @return array
+     */
+    public function asArray()
+    {
+        $a = (array)$this;
+        if (!empty($a['subs'])) {
+            foreach ($a['subs'] as &$item) {
+                $item = $item->asArray();
+            }
+            unset($item);
+        }
+        return $a;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        return '['.$this->type.' token:'.$this->line.']';
+    }
+}
---
+++ tests/helpers/TokenTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @package axy\ml
+ */
+
+namespace axy\ml\tests;
+
+use axy\ml\helpers\Token;
+
+/**
+ * @coversDefaultClass axy\ml\helpers\Token
+ */
+class TokenTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::__construct
+     */
+    public function testCreate()
+    {
+        $token = new Token('header', 20);
+        $token->name = 'this is name';
+        $token->any = 'Any!';
+        $this->assertSame('header', $token->type);
+        $this->assertSame(20, $token->line);
+        $this->assertSame('this is name', $token->name);
+        $this->assertSame('Any!', $token->any);
+    }
+
+    /**
+     * @covers ::append
+     * @covers ::getSubs
+     */
+    public function testAppend()
+    {
+        $token = new Token('block');
+        $token1 = new Token('text');
+        $token2 = new Token('tag');
+        $this->assertEmpty($token->getSubs());
+        $token->append($token1);
+        $token->append($token2);
+        $this->assertEquals([$token1, $token2], $token->getSubs());
+    }
+
+    /**
+     * @covers ::asArray
+     */
+    public function testAsArray()
+    {
+        $token = new Token('block', 10);
+        $token1 = new Token('text', 11);
+        $token2 = new Token('tag');
+        $token2->any = 'Any';
+        $token->append($token1);
+        $token->append($token2);
+        $expected = [
+            'type' => 'block',
+            'line' => 10,
+            'subs' => [
+                [
+                    'type' => 'text',
+                    'line' => 11,
+                ],
+                [
+                    'type' => 'tag',
+                    'line' => null,
+                    'any' => 'Any',
+                ],
+            ],
+        ];
+        $this->assertEquals($expected, $token->asArray());
+    }
+
+    /**
+     * @covers ::toString
+     */
+    public function testToString()
+    {
+        $token = new Token('block', 12);
+        $this->assertSame('[block token:12]', ''.$token);
+    }
+}
