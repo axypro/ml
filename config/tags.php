081c288e099758ea2b6e5f20e67cdc56c2c4c81f
Oleg Grigoriev <go.vasac@gmail.com>
2014-01-11 18:00:04 +0400

    Config: list of tags

---
+++ config/tags.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The list of tags
+ *
+ * @package axy\ml
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+
+return [
+    'b' => 'HtmlTag',
+    'i' => 'HtmlTag',
+    'u' => 'HtmlTag',
+    'br' => 'HtmlTag',
+    'ul' => 'HtmlTag',
+    'ol' => 'HtmlTag',
+    'li' => 'HtmlTag',
+    'table' => 'HtmlTag',
+    'tr' => 'HtmlTag',
+    'td' => 'HtmlTag',
+    '/' => 'ClosingTag',
+    'http' => 'Scheme',
+    'https' => 'Scheme',
+    'tag' => 'Tag',
+    'url' => 'Url',
+    'code' => 'Code',
+];
