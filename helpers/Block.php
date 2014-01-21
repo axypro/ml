fb32d08e8be19fb8aab87f970726d5bfea5c9dbc
Oleg Grigoriev <go.vasac@gmail.com>
2014-01-21 14:11:04 +0400

    ref

--- helpers/Block.php
+++ helpers/Block.php
@@ -16,6 +16,13 @@ use axy\callbacks\Callback;
 class Block
 {
     /**
+     * The block content that has already been rendered
+     *
+     * @var string
+     */
+    public $content;
+
+    /**
      * Construct
      *
      * @param \axy\ml\helpers\Token $container
@@ -40,19 +47,19 @@ class Block
     {
         $context = $this->context;
         $options = $context->options->getSource();
+        $content = &$this->content;
+        $content = '';
         $tags = $context->tags;
         $blocks = [];
-        $block = [];
-        $lastCr = true;
         $listlevel = 0;
         $list = null;
         $lists = [];
         $lnl = $options['beauty'] ? "\n" : '';
         foreach ($this->container->subs as $token) {
+            $crblock = true;
             switch ($token->type) {
                 case Token::TYPE_TEXT:
-                    $lastCr = true;
-                    $block[] = Highlight::text($token->content, $options);
+                    $content .= Highlight::text($token->content, $options);
                     break;
                 case Token::TYPE_TAG:
                     $tag = $tags->create($token->name, $token->content, $context);
@@ -65,23 +72,25 @@ class Block
                             ];
                             $context->errors[] = new Error(Error::TAG_INVALID, $token->line, $data);
                         }
-                        if (empty($block)) {
-                            $block[] = $html;
+                        if ($content === '') {
+                            $content = $html;
+                            if (!$tag->shouldCreateBlock()) {
+                                $crblock = false;
+                            }
                         } else {
                             if ($tag->shouldSplitBlock()) {
                                 foreach (\array_reverse($lists) as $l) {
-                                    $block[] = '</li>'.$lnl.'</'.$l.'>'.$lnl;
+                                    $content .= '</li>'.$lnl.'</'.$l.'>'.$lnl;
                                 }
                                 $listlevel = 0;
                                 $lists = [];
-                                $blocks[] = self::wrapBlock($block, $options, $lastCr);
-                                $blocks[] = self::wrapBlock([$html], $options, $tag->shouldCreateBlock());
-                                $block = [];
+                                $blocks[] = self::wrapBlock($content, $options, $crblock);
+                                $blocks[] = self::wrapBlock($html, $options, $tag->shouldCreateBlock());
+                                $content = '';
                             } else {
-                                $block[] = $html;
+                                $content .= $html;
                             }
                         }
-                        $lastCr = $tag->shouldCreateBlock();
                     } else {
                         $data = [
                             'tag' => $token->name,
@@ -95,50 +104,49 @@ class Block
                         $list = ($token->start === null) ? 'ul' : 'ol';
                         for ($i = 0; $i < $delta; $i++) {
                             $s = ($token->start > 1) ? ' start="'.$token->start.'"' : '';
-                            $block[] = (empty($block) ? '' : $lnl).'<'.$list.$s.'>'.$lnl.'<li>';
+                            $content .= (($content !== '') ? $lnl : '').'<'.$list.$s.'>'.$lnl.'<li>';
                             $lists[] = $list;
                         }
                     } elseif ($delta < 0) {
                         $rlists = \array_reverse(\array_slice($lists, $token->level));
                         $lists = \array_slice($lists, 0, $token->level);
                         foreach ($rlists as $l) {
-                            $block[] = '</li>'.$lnl.'</'.$l.'>';
+                            $content .= '</li>'.$lnl.'</'.$l.'>';
                         }
-                        $block[] = '</li>'.$lnl.'<li>';
+                        $content .= '</li>'.$lnl.'<li>';
                     } else {
-                        $block[] = '</li>'.$lnl.'<li>';
+                        $content .= '</li>'.$lnl.'<li>';
                     }
                     $listlevel = $token->level;
                     break;
             }
         }
-        if (!empty($block)) {
+        if ($content !== '') {
             foreach (\array_reverse($lists) as $l) {
-                $block[] = '</li>'.$lnl.'</'.$l.'>';
+                $content .= '</li>'.$lnl.'</'.$l.'>';
             }
-            $blocks[] = self::wrapBlock($block, $options, $lastCr);
+            $blocks[] = self::wrapBlock($content, $options, $crblock);
         }
         return $blocks;
-
     }
 
     /**
      * @param array $block
      * @param array $options
-     * @param boolean $lastCr
+     * @param boolean $crblock
      * @return array
      */
-    private static function wrapBlock(array $block, $options, $lastCr)
+    private static function wrapBlock($content, $options, $crblock)
     {
-        if ((!$lastCr) && (\count($block) === 1)) {
-            return $block[0];
+        $content = \trim($content);
+        if ((!$crblock) || ($content === '')) {
+            return $content;
         }
-        $block = \trim(\implode('', $block));
         if ($options['bHandler']) {
-            return Callback::call($options['bHandler'], [$block]);
+            return Callback::call($options['bHandler'], [$content]);
         }
         $t = $options['bTags'];
-        return $t[0].$block.$t[1];
+        return $t[0].$content.$t[1];
     }
 
     /**
